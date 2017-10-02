956873273938347c03cb6f0f36ba0bc136276e0a
rmishra <rmishra@RMISHRA01-LT.vocera.local>
2017-10-01 17:22:45 -0700

    Adding application class

---
+++ Airlines/src/main/java/com/ravi/app/AirlinesApp.java
@@ -0,0 +1,11 @@
+package com.ravi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesApp {
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinesApp.class, args);
+	}
+}
