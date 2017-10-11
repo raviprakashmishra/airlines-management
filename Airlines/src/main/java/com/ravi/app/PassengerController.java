1f1c76efcabb24f2ea070b1cd231ba1e16f55720
rmishra <rmishra@RMISHRA01-LT.vocera.local>
2017-10-11 12:22:15 -0700

    moving passenger controller

---
+++ Airlines/src/main/java/com/ravi/app/PassengerController.java
@@ -0,0 +1,25 @@
+package com.ravi.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ravi.model.Passenger;
+import com.ravi.service.PassengerService;
+
+@RestController
+public class PassengerController {
+
+	@Autowired
+	private PassengerService passengerService;
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/passenger", params = {"firstname", "lastname",
+			"age","gender"})
+	public Passenger createPassenger(@RequestParam("firstname") String firstName, @RequestParam("lastname") String lastName,
+			@RequestParam("age") int age, @RequestParam("gender") String gender) {
+		passengerService.createPassenger(firstName, lastName, age, gender);
+		return null;
+	}
+}
