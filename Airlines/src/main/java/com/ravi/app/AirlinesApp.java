4981df88e7196c51d2cc75943d4c7ed44a789db0
rmishra <rmishra@RMISHRA01-LT.vocera.local>
2017-10-16 00:16:58 -0700

    added scanbase package and cassandra repository annotations

--- Airlines/src/main/java/com/ravi/app/AirlinesApp.java
+++ Airlines/src/main/java/com/ravi/app/AirlinesApp.java
@@ -2,8 +2,10 @@ package com.ravi.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.ravi.service"})
+@EnableCassandraRepositories("com.ravi.service")
 public class AirlinesApp {
 	public static void main(String[] args) {
 		SpringApplication.run(AirlinesApp.class, args);
