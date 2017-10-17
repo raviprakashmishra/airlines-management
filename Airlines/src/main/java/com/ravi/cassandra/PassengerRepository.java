60ee5682ebd472e968d23dfe4f03f2e19d0935d6
rmishra <rmishra@RMISHRA01-LT.vocera.local>
2017-10-17 12:02:06 -0700

    Passenger repository implements CrudRepository

--- Airlines/src/main/java/com/ravi/cassandra/PassengerRepository.java
+++ Airlines/src/main/java/com/ravi/cassandra/PassengerRepository.java
@@ -1,10 +1,11 @@
-package com.ravi.service;
+package com.ravi.cassandra;
 
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
 
 import com.ravi.model.Passenger;
+
 
 public interface PassengerRepository extends CrudRepository<Passenger, UUID>{
 
