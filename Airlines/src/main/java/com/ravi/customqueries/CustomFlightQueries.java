168915aca82f3d42df5e715c647c4d9d9a5b1876
rmishra <rmishra@RMISHRA01-LT.vocera.local>
2017-10-29 19:22:32 -0700

    added logic to search flight bw to and from

--- Airlines/src/main/java/com/ravi/customqueries/CustomFlightQueries.java
+++ Airlines/src/main/java/com/ravi/customqueries/CustomFlightQueries.java
@@ -5,13 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.cassandra.core.CassandraTemplate;
 
+import com.datastax.driver.core.querybuilder.Clause;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.datastax.driver.core.querybuilder.Select;
 import com.ravi.model.Flight;
 
 public class CustomFlightQueries {
 	@Autowired
 	CassandraTemplate temp;
 	
-	public List<Flight> seachFlights(){
-		return null;
+	public List<Flight> seachFlights(String to, String from){
+		Clause eq1 = QueryBuilder.eq("to", to);
+		Clause eq2 = QueryBuilder.eq("from", from);
+		
+		
+		Select select = QueryBuilder.select().from("Flight");
+		select.where(eq1).and(eq2);
+				  
+		
+		List<Flight> list = temp.select(select, Flight.class);
+		return list;
 	}
 }
