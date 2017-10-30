f2e78fef6054c27905f0580d12e0d7245657726b
rmishra <rmishra@RMISHRA01-LT.vocera.local>
2017-10-29 19:26:14 -0700

    wired flight service

--- Airlines/src/main/java/com/ravi/controller/FlightController.java
+++ Airlines/src/main/java/com/ravi/controller/FlightController.java
@@ -2,16 +2,19 @@ package com.ravi.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ravi.model.Flight;
+import com.ravi.service.FlightService;
 
 @RestController
 public class FlightController {
-	
+	@Autowired
+	FlightService service;
 	
 	public List<Flight> searchFlights(String from, String to) {
-		return null;
+		return service.getFlight(to, from);
 	}
 	
 }
