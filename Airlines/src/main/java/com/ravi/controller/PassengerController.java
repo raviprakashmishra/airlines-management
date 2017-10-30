667f5f0b0e16c8f2f0e29103c922756db391c164
rmishra <rmishra@RMISHRA01-LT.vocera.local>
2017-10-29 18:17:55 -0700

    added controller for fetching a passenger record

--- Airlines/src/main/java/com/ravi/controller/PassengerController.java
+++ Airlines/src/main/java/com/ravi/controller/PassengerController.java
@@ -1,7 +1,11 @@
 package com.ravi.controller;
 
 
+
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +25,14 @@ public class PassengerController {
 	public Passenger createPassenger(@RequestParam("firstname") String firstName, @RequestParam("lastname") String lastName,
 			@RequestParam("age") int age, @RequestParam("gender") String gender) {
 		Passenger passenger = passengerService.createPassenger(firstName, lastName, age, gender);
+		return passenger;
+	}
+	
+	
+	@RequestMapping(method  = RequestMethod.GET, value = "/passenger/{id}")
+	public Passenger getPassenger(@PathVariable(value="id") String id ) {
+		UUID passID = UUID.fromString(id);
+		Passenger passenger = passengerService.getPassenger(passID);		
 		return passenger;
 	}
 	
