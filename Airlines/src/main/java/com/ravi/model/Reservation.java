c78423d90969fd920da2a3eaefbb7bcf320b4071
rmishra <rmishra@RMISHRA01-LT.vocera.local>
2017-10-29 19:27:41 -0700

    updated reservation

--- Airlines/src/main/java/com/ravi/model/Reservation.java
+++ Airlines/src/main/java/com/ravi/model/Reservation.java
@@ -1,7 +1,37 @@
 package com.ravi.model;
 
+import java.util.List;
+
 public class Reservation {
-	private String orderNumber;
-	private Passenger passenger;
-	private int price;
+	private String 			orderNumber;
+	private Passenger 		passenger;
+	private int			 	price;
+	private List<Flight> 	flights;
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	public Passenger getPassenger() {
+		return passenger;
+	}
+	public void setPassenger(Passenger passenger) {
+		this.passenger = passenger;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public List<Flight> getFlights() {
+		return flights;
+	}
+	public void setFlights(List<Flight> flights) {
+		this.flights = flights;
+	}
+	
+	
+	
 }
